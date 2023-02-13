26ae66e515fe80599bbe37e0695b2b55cd5c44d4
tawfiq <tawfiq.khnicha@menaps.com>
2023-02-13 23:19:51 +0100

    button animation

--- src/common/components/button/Button.tsx
+++ src/common/components/button/Button.tsx
@@ -1,12 +1,28 @@
 import Link from "next/link";
+import { useRef, useState } from "react";
 import { IButton } from "./IButton";
 
 export default function Button({title, href, action, animated, className}: IButton){
+
+    const btn = useRef(null)
+    let x, y;
+
+    const [coordonate, setCoordonate] = useState({x: 0, y:0})
+
+    const handleMouseOver = (e: any) =>{
+             x = e.pageX - btn.current?.offsetLeft;
+             y = e.pageY - btn.current?.offsetTop;
+
+             setCoordonate({x:x, y:y})
+
+             console.log(coordonate)
+            
+    }
     return (
         <>
-            <div className={`master-button ${className ? className : ""}`}>
+            <div ref={btn}  className={`master-button ${className ? className : ""}`} onMouseOver={()=>handleMouseOver(event)}>
                 <Link href={href} className={`text-decoration-none ${animated ? "animated-link" : ""}`}>  {title} 
-                {animated && <span className="hover-effect" ></span>}
+                {animated && <span className="hover-effect" style={{top: coordonate.y, left: coordonate.x}}></span>}
                 </Link>
             </div> 
         </>
