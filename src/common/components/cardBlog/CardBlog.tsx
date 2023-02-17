955371f50a3bd87dbb90d2b1b1e7cc4f459c6d1d
malek.dahmeni <malek.dahmeni@menaps.com>
2023-02-17 12:01:31 +0100

    wip

--- src/common/components/cardBlog/CardBlog.tsx
+++ src/common/components/cardBlog/CardBlog.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Button from "../button/Button"
 import { ICardBlog } from "./ICardBlog"
 
-export default function CardBlog({ imgsrc, Cardtitle, content ,href  }: ICardBlog) {
+export default function CardBlog({ imgsrc, Cardtitle, content ,href,tag  }: ICardBlog) {
 
     const [isOpen, setOpen] = useState(false)
 
@@ -11,12 +11,13 @@ export default function CardBlog({ imgsrc, Cardtitle, content ,href  }: ICardBlo
     }
 
     return (
-        <div className="card"  >
+        <div className="card cardstyle shadow"  >
+            <div className="textstyleblog rounded">{tag}</div>
             <img className="card-img-top" src= {imgsrc}  alt="Card image cap" />
             <div className="card-body">
                 <h5 className="  categoryTitle">{Cardtitle}</h5>
                 <p className="  categoryContent">{content}</p>
-                <a href={href} className="btn btn-primary">READ MORE »</a>
+                <a href={href} className="  readmore">READ MORE »</a>
             </div>
         </div>
 
